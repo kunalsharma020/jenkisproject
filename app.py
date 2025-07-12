1a81fbcd8dfe12126d0679ce0f4ab5797bdc392c
Kunal Sharma <kunalsharmakp031@gmail.com>
2025-07-12 17:21:49 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, this is a Flask web app!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
